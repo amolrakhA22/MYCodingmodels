81e740250d489cd94fa24ff582a765e3fde327c9
amolrakhA22 <127944734+amolrakhA22@users.noreply.github.com>
2023-09-16 13:44:02 +0530

    Add files via upload

--- C++ Code/Odd_Even A22.cpp
+++ C++ Code/Odd_Even A22.cpp
@@ -1,1 +1,32 @@
-All Basic codes in C++ Programming language 
+// C++ Program using oop's concept for odd_even number  
+// By Amol Rakh
+
+#include <iostream>
+using namespace std;
+ 
+ class amol //class
+ {
+    public:
+    int num;
+    int odd_even() //function for odd even number
+    {
+        cout<<"Enter a number :";
+        cin>>num;
+        if ( num % 2 == 0) //condition for comparing number
+        {
+            cout<<num<<" is even number";
+        }
+        else
+        {
+            cout<<num<<" is odd number";
+        }
+        return 0;
+    }
+ };
+ 
+ int main()
+ {
+     amol rakh; //giving name to class as 'rakh'
+     rakh.odd_even(); //calling Function from class amol
+     return 0;
+ }
